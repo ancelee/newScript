d5c4f3f9abe0d32e046a1642edfd122982ae380b
ancelee <ancelee@163.com>
2021-06-11 16:51:31 +0800

    添加古蒂安_pk修改脚本

--- jdFruitShareCodes.js
+++ jdFruitShareCodes.js
@@ -7,8 +7,8 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let FruitShareCodes = [
-  '0a74407df5df4fa99672a037eec61f7e@dbb21614667246fabcfd9685b6f448f3@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@56db8e7bc5874668ba7d5195230d067a',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@9c52670d52ad4e1a812f894563c746ea@8175509d82504e96828afc8b1bbb9cb3',//账号二的好友shareCode，不同好友中间用@符号隔开
+  '0a6b579e87474e7ab93ad71af0dde5ab@60799780f898446eb9f427fe1fe8f43b@1465ccd3d1664f60adc436466ca5f222@2ddf6c5a0c174241aaf0c319604718bf',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '0a6b579e87474e7ab93ad71af0dde5ab@60799780f898446eb9f427fe1fe8f43b@1465ccd3d1664f60adc436466ca5f222@2ddf6c5a0c174241aaf0c319604718bf',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 
 // 从日志获取互助码
